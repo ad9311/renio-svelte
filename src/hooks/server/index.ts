fefd1d9059a9fa1ea25c50c37fca02cc18d2415c
Ángel Díaz <ad9311@proton.me>
2024-10-05 10:09:26 -0500

    Implement server hook
    Redirect user when not authenticated

---
+++ src/hooks/server/index.ts
@@ -0,0 +1,23 @@
+import type { RequestEvent } from '@sveltejs/kit';
+import { PUBLIC_HOST } from '$env/static/public'
+
+type HandleParams = {
+	event: RequestEvent;
+	resolve: (event: RequestEvent) => Promise<Response>;
+};
+
+export async function handle({ event, resolve }: HandleParams) {
+	const pathname: string = event.url.pathname;
+	const session = undefined;
+
+	if (pathname.startsWith('/auth') && session) {
+		return Response.redirect(`${PUBLIC_HOST}`, 302);
+	}
+
+	if (!pathname.startsWith('/auth') && !session) {
+		return Response.redirect(`${PUBLIC_HOST}/auth/sign-in`, 302);
+	}
+
+	const response = await resolve(event);
+	return response;
+}
--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		files: {
+			hooks: {
+				universal: 'src/hooks/index.ts',
+				server: 'src/hooks/server/index.ts',
+				client: 'src/hooks/client/index.ts'
+			}
+		}
 	}
 };
 
