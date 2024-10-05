9a91ef894ec7fc28dc026969f24f134b15b90c6a
Ángel Díaz <ad9311@proton.me>
2024-10-05 07:24:34 -0500

    Update eslint and prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import eslint from '@eslint/js';
 import prettier from 'eslint-config-prettier';
+import i from 'eslint-plugin-import';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -10,6 +11,11 @@ export default tseslint.config(
 	...svelte.configs['flat/recommended'],
 	prettier,
 	...svelte.configs['flat/prettier'],
+	{
+		plugins: {
+			import: i
+		}
+	},
 	{
 		languageOptions: {
 			globals: {
@@ -28,5 +34,21 @@ export default tseslint.config(
 	},
 	{
 		ignores: ['build/', '.svelte-kit/', 'dist/']
+	},
+	{
+		rules: {
+			'no-else-return': 'error',
+			eqeqeq: 'error',
+			'no-console': 'warn',
+			curly: 'error',
+			'import/order': [
+				'error',
+				{
+					groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+					'newlines-between': 'always',
+					alphabetize: { order: 'asc', caseInsensitive: true }
+				}
+			]
+		}
 	}
 );
