d7961b8771d1d4863cbf64ca709b0f4086538a3c
Ángel Díaz <ad9311@proton.me>
2024-10-06 16:43:45 -0500

    Add styles and sections to sidebar

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import MainNavbar from './MainNavbar.svelte';
+	import MainNavbar from '$lib/components/MainNavbar.svelte';
 </script>
 
 <header>
 	<MainNavbar />
 </header>
-<main class="p-4">
+<main class="mt-4 container mx-auto">
 	<slot />
 </main>
