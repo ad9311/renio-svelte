49d1a12dcce06b6de1e45bcad71286fecc8dd0fa
Ángel Díaz <ad9311@proton.me>
2024-10-12 12:40:08 -0500

    Implement verify token in handle hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,18 @@
 import type { Handle, HandleFetch } from '@sveltejs/kit';
 
 import { PUBLIC_HOST } from '$env/static/public';
+import { isSessionTokenValid } from '$lib/server';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const pathname: string = event.url.pathname;
 	const sessionToken = event.cookies.get('renio-session');
+	const sessionValid = await isSessionTokenValid(sessionToken as string);
 
-	if (pathname.startsWith('/auth/sign-in') && sessionToken) {
+	if (pathname.startsWith('/auth/sign-in') && sessionValid) {
 		return Response.redirect(`${PUBLIC_HOST}`, 302);
 	}
 
-	if (!pathname.startsWith('/auth/sign-in') && !sessionToken) {
+	if (!pathname.startsWith('/auth/sign-in') && !sessionValid) {
 		return Response.redirect(`${PUBLIC_HOST}/auth/sign-in`, 302);
 	}
 
--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -1,6 +1,18 @@
+import { SESSION_KEY } from "$env/static/private";
+import { jwtVerify } from "jose";
+
 export function retrieveSessionToken(response: Response) {
 	const authHeader = response.headers.get('authorization');
 	const sessionToken = authHeader?.split(' ')[1];
 
 	return sessionToken;
+}
+
+export async function isSessionTokenValid(sessionToken: string) {
+	try {
+		await jwtVerify(sessionToken as string, new TextEncoder().encode(SESSION_KEY));
+		return true;
+	} catch (error) {
+		return false;
+	}
 }
